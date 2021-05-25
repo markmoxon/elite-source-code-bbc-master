63d57bbf0e3bd847c7694cbd4321bd56737c74b4
Mark Moxon <mark@moxon.net>
2021-05-25 12:58:15 +0100

    Minor fixes to commentary and README

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ See the [introduction](#introduction) for more information.
   * [Verifying the output](#verifying-the-output)
   * [Log files](#log-files)
 
-* [Building different releases of Elite](#building-different-releases-of-elite)
+* [Building different releases of BBC Master Elite](#building-different-releases-of-bbc-master-elite)
 
   * [Building the Master Compact release](#building-the-master-compact-release)
   * [Building the SNG47 release](#building-the-sng47-release)
@@ -215,7 +215,7 @@ All the compiled binaries match the extracts, so we know we are producing the sa
 
 During compilation, details of every step are output in a file called `compile.txt` in the `output` folder. If you have problems, it might come in handy, and it's a great reference if you need to know the addresses of labels and variables for debugging (or just snooping around).
 
-## Building different releases of Elite
+## Building different releases of BBC Master Elite
 
 This repository contains the source code for two different releases of BBC Master Elite:
 
--- sources/elite-source.asm
+++ sources/elite-source.asm
@@ -20502,6 +20502,7 @@ LOAD_C% = LOAD% +P% - CODE%
                         \
                         \   (A ?) = A * Q
                         \         = K * sin(A) * 256
+                        \
                         \ which is equivalent to:
                         \
                         \   A = K * sin(A)
@@ -21246,6 +21247,11 @@ LOAD_C% = LOAD% +P% - CODE%
 
  LDA SZ,Y               \ Fetch the Y-th dust particle's z_hi coordinate into A
 
+                        \ Fall through into DV41 to do:
+                        \
+                        \   (P R) = 256 * DELTA / A
+                        \         = 256 * DELTA / Y-th stardust particle's z_hi
+
 \ ******************************************************************************
 \
 \       Name: DV41
@@ -21279,6 +21285,11 @@ LOAD_C% = LOAD% +P% - CODE%
  STA Q                  \ Store A in Q
 
  LDA DELTA              \ Fetch the speed from DELTA into A
+
+                        \ Fall through into DVID4 to do:
+                        \
+                        \   (P R) = 256 * A / Q
+                        \         = 256 * DELTA / A
 
 \ ******************************************************************************
 \
