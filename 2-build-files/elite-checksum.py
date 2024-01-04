69ff05eb11cd18481232060a9352f4de438371f3
Mark Moxon <mark@moxon.net>
2024-01-04 10:12:20 +0000

    Honour the configuration setting for QUIET/LOUD when starting game

--- 2-build-files/elite-checksum.py
+++ 2-build-files/elite-checksum.py
@@ -46,12 +46,12 @@ print("Encryption = ", Encrypt)
 
 if release == 1:
     # SNG47
-    f = 0x7F0A                  # F%
+    f = 0x7F19                  # F%
     scramble_from = 0x2CC1      # G%
     na2_per_cent = 0x350A       # NA2%
 elif release == 2:
     # Compact
-    f = 0x7FAF                  # F%
+    f = 0x7FBE                  # F%
     scramble_from = 0x2CC1      # G%
     na2_per_cent = 0x350A       # NA2%
 
