08ad3b3f221e60340d1a0164413575acb76ed943
Mark Moxon <mark@moxon.net>
2024-01-04 15:52:22 +0000

    Insta-dock when pressing J when the docking computer is activated and we're in the safe zone

--- 2-build-files/elite-checksum.py
+++ 2-build-files/elite-checksum.py
@@ -46,12 +46,12 @@ print("Encryption = ", Encrypt)
 
 if release == 1:
     # SNG47
-    f = 0x7F90                  # F%
+    f = 0x7F9B                  # F%
     scramble_from = 0x2CBE      # G%
     na2_per_cent = 0x3528       # NA2%
 elif release == 2:
     # Compact
-    f = 0x7F7B                  # F%
+    f = 0x7F86                  # F%
     scramble_from = 0x2CBE      # G%
     na2_per_cent = 0x3528       # NA2%
 
