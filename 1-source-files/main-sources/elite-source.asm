15df24c9333d1be05f66d4528eb2cc1a6494434e
Mark Moxon <mark@moxon.net>
2024-04-09 14:37:04 +0100

    Fixed minor issues in commentary

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -4608,7 +4608,7 @@ ENDIF
  ADC Q
  STA S
 
- BCC LI210              \ If the addition didn't overflow, jump to L2110
+ BCC LI210              \ If the addition didn't overflow, jump to LI210
 
  CLC                    \ Otherwise we just overflowed, so clear the C flag and
  INY                    \ increment Y to move to the pixel line below
@@ -47891,7 +47891,7 @@ IF _COMPACT
  INX                    \ Set X = X + 1
 
  LDA JSTE               \ JSTE contains &FF if both joystick channels are
- BEQ TT171              \ reversed and 0 otherwise, so skip to L7F80 if the
+ BEQ TT171              \ reversed and 0 otherwise, so skip to TT171 if the
                         \ joystick channels are not reversed
 
  TXA                    \ The X channel needs to be reversed, so negate the
