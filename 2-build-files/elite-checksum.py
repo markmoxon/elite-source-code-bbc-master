68ec4481e7f57f9be8b81314801d0d7c591af1c9
Mark Moxon <mark@moxon.net>
2024-04-17 13:53:12 +0100

    Add logic to reduce transmission calls when buying and selling cargo and equipment

--- 2-build-files/elite-checksum.py
+++ 2-build-files/elite-checksum.py
@@ -51,7 +51,7 @@ if release == 1:
     na2_per_cent = 0x34CD       # NA2%
 elif release == 2:
     # Compact
-    f = 0x7FC7                  # F%
+    f = 0x7FCF                  # F%
     scramble_from = 0x2CC1      # G%
     na2_per_cent = 0x34CD       # NA2%
 
