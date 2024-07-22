a77c991da90d98838f4d9273c9b334b05c7d1c0b
Mark Moxon <mark@moxon.net>
2024-07-22 13:42:38 +0100

    Add a deep dive reference to MV40

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -46401,6 +46401,7 @@ ENDMACRO
 \   Category: Moving
 \    Summary: Rotate the planet or sun's location in space by the amount of
 \             pitch and roll of our ship
+\  Deep dive: Rotating the universe
 \
 \ ------------------------------------------------------------------------------
 \
