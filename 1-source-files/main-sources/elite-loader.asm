2297421a6e7d465b51217ae7d75a17256909bc0b
Mark Moxon <mark@moxon.net>
2024-12-05 14:29:20 +0000

    Update the source file headers to say whether it's  original source or a disassembly

--- 1-source-files/main-sources/elite-loader.asm
+++ 1-source-files/main-sources/elite-loader.asm
@@ -5,7 +5,7 @@
 \ BBC Master Elite was written by Ian Bell and David Braben and is copyright
 \ Acornsoft 1986
 \
-\ The code on this site has been reconstructed from a disassembly of the version
+\ The code in this file has been reconstructed from a disassembly of the version
 \ released on Ian Bell's personal website at http://www.elitehomepage.org/
 \
 \ The commentary is copyright Mark Moxon, and any misunderstandings or mistakes
